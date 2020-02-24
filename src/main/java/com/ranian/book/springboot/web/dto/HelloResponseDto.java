b1f01eee14c2b7f5ef27deca3b983916c2c072d5
ranian129 <ranian129@gmail.com>
2020-02-24 20:25:13 +0900

    add lombok library. add dto test by lombok.

--- src/main/java/com/ranian/book/springboot/web/dto/HelloResponseDto.java
+++ src/main/java/com/ranian/book/springboot/web/dto/HelloResponseDto.java
@@ -1,9 +1,18 @@
 package com.ranian.book.springboot.web.dto;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 /**
  * Created by ranian129@gmail.com on 2020-02-24
  * Blog : http://76jin.tistory.com
  * Github : http://github.com/76jin
  */
+@Getter
+@RequiredArgsConstructor
 public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
 }
