2bc6568e1bcc4bb22fcd6e848da7b5376cb86132
ranian129 <ranian129@gmail.com>
2020-02-24 20:24:14 +0900

    Remove .gradle

---
+++ src/main/java/com/ranian/book/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,9 @@
+package com.ranian.book.springboot.web.dto;
+
+/**
+ * Created by ranian129@gmail.com on 2020-02-24
+ * Blog : http://76jin.tistory.com
+ * Github : http://github.com/76jin
+ */
+public class HelloResponseDto {
+}
---
+++ src/test/java/com/ranian/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,12 @@
+package com.ranian.book.springboot.web.dto;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ranian129@gmail.com on 2020-02-24
+ * Blog : http://76jin.tistory.com
+ * Github : http://github.com/76jin
+ */
+public class HelloResponseDtoTest {
+
+}
